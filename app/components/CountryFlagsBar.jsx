2949e8e61e28fba64851959e78867595ceed537a
Sami Ktami <ktami.sami@icloud.com>
2026-01-02 20:19:07 +0100

    Fix SSR import for react-country-flag

--- app/components/CountryFlagsBar.jsx
+++ app/components/CountryFlagsBar.jsx
@@ -1,11 +1,21 @@
-import ReactCountryFlag from "react-country-flag";
+import ReactCountryFlagImport from "react-country-flag";
 import { supportedCountries, countryNames } from "../constants/supportedCountries";
 
+const ReactCountryFlag =
+  ReactCountryFlagImport?.default || ReactCountryFlagImport;
+
 export default function CountryFlagsBar() {
+  // إذا ما كانش component صحيح، مانكسرش الصفحة
+  if (typeof ReactCountryFlag !== "function") return null;
+
   return (
     <div className="tf-flags" title="Pays supportés">
       {supportedCountries.map((code) => (
-        <span key={code} className="tf-flag-item" title={countryNames[code] || code}>
+        <span
+          key={code}
+          className="tf-flag-item"
+          title={countryNames[code] || code}
+        >
           <ReactCountryFlag
             svg
             countryCode={code}
